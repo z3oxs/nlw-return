737e5c7f1415b526f6b234179b593c5a45c9df23
z3oxs <z3ox1s@protonmail.com>
2022-05-03 19:02:52 -0300

    Dia 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 module.exports = {
     content: ["./src/**/*.tsx"],
-  theme: {
-      extend: {
-          colors: {
-              brand: {
-                  500: "#8257e6"
+    theme: {
+          extend: {
+              colors: {
+                  brand: {
+                      300: '#996dff',
+                      500: "#8257e6"
+                  }
+              },
+              borderRadius: {
+                  md: '4px',
               }
-          }
-      },
-  },
-  plugins: [],
+          },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('tailwind-scrollbar'),
+    ],
 }
